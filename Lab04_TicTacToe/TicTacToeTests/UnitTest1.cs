37b8845b309a5b8428fec1a0175c9d1e5997c4bd
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-07 22:36:28 -0500

    theories written for input matches

--- Lab04_TicTacToe/TicTacToeTests/UnitTest1.cs
+++ Lab04_TicTacToe/TicTacToeTests/UnitTest1.cs
@@ -1,11 +1,12 @@
 using System;
 using Xunit;
+using Xunit.Sdk;
 
 namespace Lab04_TicTacToe.Classes
 {
     public class TicTacToeTests
     {
-        
+
         [Fact]
         public void ReturnsWinnerTest()
         {
@@ -22,7 +23,7 @@ namespace Lab04_TicTacToe.Classes
 
             // Assert
             Assert.True(winnerExists);
-            
+
         }
 
 
@@ -30,45 +31,51 @@ namespace Lab04_TicTacToe.Classes
         public void Can_switch_player()
         {
             // Arrange
-            Player player1 = new Player();
-            Player player2 = new Player();
+            Player player1 = new Player() { Name = "Ted" };
+            Player player2 = new Player() { Name = "Bill" };
 
             Game game = new Game(player1, player2);
-            
-
 
 
             // Act
             Player canSwitch = game.NextPlayer();
+            Player canSwitchBack = game.NextPlayer();
 
             // Assert
             Assert.Equal(player2, canSwitch);
+            Assert.Equal(player2, canSwitchBack);
 
         }
 
-        [Fact]
-        public void Player_input_matches_index()
+        [Theory]
+        [InlineData(1, 0, 0)]
+        [InlineData(2, 0, 1)]
+        [InlineData(3, 0, 2)]
+        [InlineData(4, 1, 0)]
+        [InlineData(5, 1, 1)]
+        [InlineData(6, 1, 2)]
+        [InlineData(7, 2, 0)]
+        [InlineData(8, 2, 1)]
+        [InlineData(9, 2, 2)]
+
+        public void Player_input_matches_index(int value, int expectedRow, int expectedCol)
         {
             // Arrange
             Player player1 = new Player();
             Player player2 = new Player();
 
-            //Game game = new Game(player1, player2);
-            Board board = new Board();
-
-
 
             // Act
-            Position actual = Player.PositionForNumber(1);
+            Position actual = Player.PositionForNumber(value);
 
 
 
             // Assert
             Assert.NotNull(actual);
-            Assert.Equal(0, actual.Row);
-            Assert.Equal(0, actual.Column);
+            Assert.Equal(expectedRow, actual.Row);
+            Assert.Equal(expectedCol, actual.Column);
 
         }
 
-        }
     }
+}
