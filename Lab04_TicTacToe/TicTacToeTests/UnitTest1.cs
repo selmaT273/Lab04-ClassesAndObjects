f78aefd4530e5536748f78c6f0d06bf00908956d
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-07 22:11:23 -0500

    test passes for verifying matrix coordinates

--- Lab04_TicTacToe/TicTacToeTests/UnitTest1.cs
+++ Lab04_TicTacToe/TicTacToeTests/UnitTest1.cs
@@ -42,8 +42,33 @@ namespace Lab04_TicTacToe.Classes
             Player canSwitch = game.NextPlayer();
 
             // Assert
-            Assert.Equal(player1, canSwitch);
+            Assert.Equal(player2, canSwitch);
+
+        }
+
+        [Fact]
+        public void Player_input_matches_index()
+        {
+            // Arrange
+            Player player1 = new Player();
+            Player player2 = new Player();
+
+            //Game game = new Game(player1, player2);
+            Board board = new Board();
+
+
+
+            // Act
+            Position actual = Player.PositionForNumber(1);
+
+
+
+            // Assert
+            Assert.NotNull(actual);
+            Assert.Equal(0, actual.Row);
+            Assert.Equal(0, actual.Column);
+
+        }
 
         }
     }
-}
